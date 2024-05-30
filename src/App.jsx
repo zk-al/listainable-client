240d8512b51c14c82fb63f4a1efc5ece8437152b
Zack Lamarche <lamarche.zack@gmail.com>
2024-05-30 10:46:00 -0700

    Add components, pages and routes :hammer:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import ListPage from "./pages/ListPage/ListPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/list" element={<ListPage />} />
+      </Routes>
+      <div className="Login"></div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
