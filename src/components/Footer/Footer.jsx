7a1ba98d7d5450a254b39cfe573ffae0aa3d3eb8
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-04 22:27:50 -0700

    Add base HTML :lipstick:

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <img src="#" alt="Home Icon" />
+        </li>
+        <li>
+          <img src="#" alt="List Icon" />
+        </li>
+        <li>
+          <img src="#" alt="Login Icon" />
+        </li>
+      </ul>
+      <div>
+        <p>Listainable all rights reserves</p>
+        <p>
+          Contains information from OpenFoodFacts, which is made available here
+          under the Open Database License (ODbL).
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
