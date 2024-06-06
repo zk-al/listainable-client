4442798a01ce10849c219898c2a6287b446d34dd
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-05 21:53:20 -0700

    Add quantity handlers and axios :hammer:

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 8080;
 const baseUrl = `http://localhost:${PORT}`;
 
 //TODO:
-// 1. Add DELETE axios handle onChange to checkbox
 // 2. Add increase to + btn - add + 1 to current qty
 // 3. Add decrease to - btn -  -1 to current qty
 // 4. Add qtyHandler - axios PUT to trigger when qty is changed - this iwll update the qty on the front end
@@ -27,21 +26,83 @@ function ListPage() {
     getList();
   }, []);
 
+  const deleteItem = async (id) => {
+    await axios.delete(`${baseUrl}/list/${id}`);
+    setUserList(userList.filter((item) => item.id !== id));
+  };
+
+  const handleCheckbox = async (id) => {
+    try {
+      deleteItem(id);
+    } catch (error) {
+      console.error("Error deleting item: ", error);
+    }
+  };
+
+  const handleQty = async (id, newQty) => {
+    try {
+      await axios.put(`${baseUrl}/list/${id}`, { quantity: newQty });
+      setUserList(
+        userList.map((listItem) =>
+          listItem.id === id ? { ...listItem, quantity: newQty } : listItem
+        )
+      );
+    } catch (error) {
+      console.error("Error adding quantity: ", error);
+    }
+  };
+
+  const increaseQty = async (id, currentQty) => {
+    try {
+      const newQty = parseInt(currentQty) + 1;
+      handleQty(id, newQty);
+    } catch (error) {
+      console.error("Error adding quantity: ", error);
+    }
+  };
+
+  const decreaseQty = async (id, currentQty) => {
+    try {
+      if (currentQty > 1) {
+        const newQty = parseInt(currentQty) - 1;
+        handleQty(id, newQty);
+      } else {
+        deleteItem(id);
+      }
+    } catch (error) {
+      console.error("Error adding quantity: ", error);
+    }
+  };
+
   return (
     <>
       <ul>
         {userList.map((listItem) => (
           <li key={listItem.id}>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={() => {
+                handleCheckbox(listItem.id);
+              }}
+            />
             <p>{listItem.product_name}</p>
             <div>
-              <button>+</button>
+              <button
+                onClick={() => increaseQty(listItem.id, listItem.quantity)}
+              >
+                +
+              </button>
               <p>{listItem.quantity}</p>
-              <button>-</button>
+              <button
+                onClick={() => decreaseQty(listItem.id, listItem.quantity)}
+              >
+                -
+              </button>
             </div>
           </li>
         ))}
       </ul>
+      <div>Average EcoScore</div>
     </>
   );
 }
