1b8744ac80bbcc44e81473fe8c78f58cb139d248
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-09 21:26:03 -0700

    Add base card styles :lipstick:

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,8 @@ import "../../styles/partials/_global.scss";
 import axios from "axios";
 import { useRef, useEffect, useState } from "react";
 import { register } from "swiper/element/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const PORT = process.env.REACT_APP_API_PORT || 8080;
 const baseUrl = `http://localhost:${PORT}`;
@@ -40,13 +42,27 @@ function HomePage() {
 
   return (
     <>
-      <section>
+      <section className="slider-section">
         <h2>Plant-Based Foods</h2>
         <swiper-container
           ref={swiperElRef}
-          slides-per-view="3"
+          slides-per-view="1"
           navigation="true"
           loop="true"
+          breakpoints='{
+            "320": {
+              "slidesPerView": 1,
+              "spaceBetween": 20
+            },
+            "600": {
+              "slidesPerView": 2,
+              "spaceBetween": 30
+            },
+            "1024": {
+              "slidesPerView": 3,
+              "spaceBetween": 40
+            }
+          }'
         >
           {products
             .filter((product) =>
@@ -54,74 +70,160 @@ function HomePage() {
             )
             .map((product) => (
               <swiper-slide key={product.id}>
-                <img
-                  src={`${baseUrl}/${product.product_image}`}
-                  alt={product.product_name}
-                />
-                <h3>{product.product_name}</h3>
+                <div class="card">
+                  <img
+                    className="card__image"
+                    src={`${baseUrl}/${product.product_image}`}
+                    alt={product.product_name}
+                  />
+                  <div className="card__text">
+                    <h3 className="card__name">{product.product_name}</h3>
+                    <h4 className="eco-score">
+                      Eco Score: {product.eco_score}
+                    </h4>
+                    <h4 className="nutri-score">
+                      Nutri Score: {product.nutri_score}
+                    </h4>
+                  </div>
+                </div>
               </swiper-slide>
             ))}
         </swiper-container>
       </section>
-      <section>
+      <section className="slider-section">
         <h2>Frozen Foods</h2>
         <swiper-container
           ref={swiperElRef}
-          slides-per-view="3"
+          slides-per-view="1"
           navigation="true"
           loop="true"
+          breakpoints='{
+            "320": {
+              "slidesPerView": 1,
+              "spaceBetween": 20
+            },
+            "600": {
+              "slidesPerView": 2,
+              "spaceBetween": 30
+            },
+            "1024": {
+              "slidesPerView": 3,
+              "spaceBetween": 40
+            }
+          }'
         >
           {products
             .filter((product) => product.categories.includes("Frozen Foods"))
             .map((product) => (
               <swiper-slide key={product.id}>
-                <img
-                  src={`${baseUrl}/${product.product_image}`}
-                  alt={product.product_name}
-                />
-                <h3>{product.product_name}</h3>
+                <div class="card">
+                  <img
+                    className="card__image"
+                    src={`${baseUrl}/${product.product_image}`}
+                    alt={product.product_name}
+                  />
+                  <div className="card__text">
+                    <h3 className="card__name">{product.product_name}</h3>
+                    <h4 className="eco-score">
+                      Eco Score: {product.eco_score}
+                    </h4>
+                    <h4 className="nutri-score">
+                      Nutri Score: {product.nutri_score}
+                    </h4>
+                  </div>
+                </div>
               </swiper-slide>
             ))}
         </swiper-container>
       </section>
-      <section>
+      <section className="slider-section">
         <h2>Snacks</h2>
         <swiper-container
           ref={swiperElRef}
-          slides-per-view="3"
+          slides-per-view="1"
           navigation="true"
           loop="true"
+          breakpoints='{
+            "320": {
+              "slidesPerView": 1,
+              "spaceBetween": 20
+            },
+            "600": {
+              "slidesPerView": 2,
+              "spaceBetween": 30
+            },
+            "1024": {
+              "slidesPerView": 3,
+              "spaceBetween": 40
+            }
+          }'
         >
           {products
             .filter((product) => product.categories.includes("Snacks"))
             .map((product) => (
               <swiper-slide key={product.id}>
-                <img
-                  src={`${baseUrl}/${product.product_image}`}
-                  alt={product.product_name}
-                />
-                <h3>{product.product_name}</h3>
+                <div class="card">
+                  <img
+                    className="card__image"
+                    src={`${baseUrl}/${product.product_image}`}
+                    alt={product.product_name}
+                  />
+                  <div className="card__text">
+                    <h3 className="card__name">{product.product_name}</h3>
+                    <h4 className="eco-score">
+                      Eco Score: {product.eco_score}
+                    </h4>
+                    <h4 className="nutri-score">
+                      Nutri Score: {product.nutri_score}
+                    </h4>
+                  </div>
+                </div>
               </swiper-slide>
             ))}
         </swiper-container>
       </section>
-      <section>
+      <section className="slider-section">
         <h2>Vegetables</h2>
         <swiper-container
           ref={swiperElRef}
-          slides-per-view="3"
+          slides-per-view="1"
           navigation="true"
           loop="true"
+          breakpoints='{
+            "320": {
+              "slidesPerView": 1,
+              "spaceBetween": 20
+            },
+            "600": {
+              "slidesPerView": 2,
+              "spaceBetween": 30
+            },
+            "1024": {
+              "slidesPerView": 3,
+              "spaceBetween": 40
+            }
+          }'
         >
           {products
             .filter((product) => product.categories.includes("Vegetables"))
             .map((product) => (
               <swpier-slide key={product.id}>
-                <img
-                  src={`${baseUrl}/${product.product_image}`}
-                  alt={product.product_name}
-                />
-                <h3>{product.product_name}</h3>
+                <div class="card">
+                  <img
+                    className="card__image"
+                    src={`${baseUrl}/${product.product_image}`}
+                    alt={product.product_name}
+                  />
+                  <div className="card__text">
+                    <h3 className="card__name">{product.product_name}</h3>
+                    <h4 className="eco-score">
+                      Eco Score: {product.eco_score}
+                    </h4>
+                    <h4 className="nutri-score">
+                      Nutri Score: {product.nutri_score}
+                    </h4>
+                  </div>
+                </div>
               </swpier-slide>
             ))}
         </swiper-container>
